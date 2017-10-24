607ec7e35f32a3fa4486a1ac6adccb04c0588bd5
Neil Aquilina <neila@mcast.org.mt>
2017-10-24 14:39:12 +0200

    Finished NUmberWizardUI and started BlockBreaker

---
+++ NumberWizardUI/NumberWizardUI/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    //creates 3 number variables
+    int min = 1;
+    int max = 1001;
+    int guess = 500;
+    int maxNumberOfTries = 10;
+
+    public Text guessText;
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+
+        guessText.text = guess.ToString();
+
+        maxNumberOfTries--;
+        if(maxNumberOfTries == 0) //if AI does not guess the number I win
+        {
+            SceneManager.LoadScene("Win");
+            min = 1;
+            max = 1001;
+            guess = Random.Range(min, max); 
+        }
+    }
+    
+    // Use this for initialization
+    void Start () {
+
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+
+
+    }
+	
+	
+}
