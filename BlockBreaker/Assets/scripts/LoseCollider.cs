09c4f412003df6d67c6a76f05ccae1009e4779a1
Neil Aquilina <neila@mcast.org.mt>
2017-11-02 13:20:51 +0100

    Updated BlockBreaker with Physics and Bouncing Ball

---
+++ BlockBreaker/Assets/scripts/LoseCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager myLevelManager;
+
+    //is called when Collision happens
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision with "+ collision.gameObject.name);
+        
+    }
+
+    //is called when Collider is a Trigger
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        myLevelManager.LoadLevel("Win");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
