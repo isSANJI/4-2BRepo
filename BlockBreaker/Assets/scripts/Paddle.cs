cc94decbbb763c16ef6e49c2de9eca4acfe5b604
Neil Aquilina <neila@mcast.org.mt>
2017-11-07 13:53:22 +0100

    Updated BlockBreaker having moving Paddle with the mouse

---
+++ BlockBreaker/Assets/scripts/Paddle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //save the mouse position in Unity units 
+        //(depending on the number of units on screen
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 13f) - 6.5f;
+
+        //creates a Vector3 which saves a coordinate
+        //in (x,y,z). 
+        //this.transform.position.y: keeps the starting y-value
+        Vector3 paddlePosition = new Vector3(2f, this.transform.position.y, 0f);
+
+        
+
+        //make the paddle move with the mouse
+        paddlePosition.x = Mathf.Clamp(mousePosInUnits, -6.5f, 6.5f);
+
+        
+
+        //set the position of the paddle (this) to paddlePosition
+        this.transform.position = paddlePosition;
+
+		
+	}
+}
